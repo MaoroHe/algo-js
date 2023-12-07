efbe08a111e3a30eed8a7117774f35893349fe11
MaoroHe <maorohecq1409@gmail.com>
2023-12-07 15:22:39 +0100

    exercise

--- 1-2.js
+++ 1-2.js
@@ -5,3 +5,16 @@ let city = 'Charleroi';
 let pres = 'My name is ' + firstName + ' ' + name + ' i live in ' + city + '.';
 
 console.log(pres);
+
+// let i = 1;
+
+// while (i <= 100) {
+//   console.log(i);
+//   i += 1;
+// }
+
+// shorter version
+
+// for (let i = 1; i <= 100; i += 1) {
+//     console.log(i);
+//   }
---
+++ 2-2.js
@@ -0,0 +1,7 @@
+let min = prompt('Enter a minimal number');
+let current = prompt('Enter your current number');
+let max = prompt('Enter your maximal number');
+
+if (min > max) {
+    window.alert("You really don't understand anything. End of program.");
+}
