d60d224f8b44bb86cb203a4c2dd365e8a0b5d480
MaoroHe <maorohecq1409@gmail.com>
2023-12-08 12:29:08 +0100

    exercise

---
+++ 4-1.js
@@ -0,0 +1,8 @@
+let x = Number(prompt('length of your rectangle:'));
+let y = Number(prompt('width of your rectangle:'));
+
+function calcSurface(a, b) {
+    return a * b
+}
+
+console.log(calcSurface(x, y))
---
+++ 4-2.js
@@ -0,0 +1,5 @@
+function rand10(max) {
+    return Math.floor(Math.random() * max);
+}
+
+console.log(rand10(10));
