8c5ce34aaf8d07b445c888840a1c425bbad63c5b
MaoroHe <maorohecq1409@gmail.com>
2023-12-11 13:19:08 +0100

    update

--- 5-1.js
+++ 5-1.js
@@ -1,24 +1,29 @@
-// const serie = prompt('Your favorite serie:');
-// const productionyear = prompt('Production year:');
-// const cast = prompt('Cast member:');
-// const tvserie = {serie: serie, production_year: productionyear, cast: cast};
-// this.cast = prompt('Membre du cast:');
-// console.log(tvserie);
+/*let serie, prod, castNumb, castName=[], cast=[]; 
+
+function askTvSerie(serie, prod, castNumb, cast) {
+    serie = prompt('Nom de votre série favorite?');
+    prod = prompt('Année de production de votre série favorite?');
+    castNumb = prompt('Combien de membre du cast voulez vous entrez?');
+
+    for (let i = 0; i < castNumb; i++) {
+        cast = prompt('Nom du cast?');
+        castName.push(cast)
+        cast = castName;
+    };
+
+    let tvSerie = {
+        serie: serie,
+        production_year: prod,
+        cast_member: cast,
+    };
+
+    return tvSerie;
+}
+
+console.log(askTvSerie());*/
 
 
-// let tvSerie;
-
-// function askTvSerie(tvSerie) {
-//     tvSerie = {
-//         serie: prompt("Série préférée?"),
-//         production: prompt("Année de production?"),
-//         cast: prompt("Membre du cast?"),
-//     };
-// }
-
-// askTvSerie()
-// console.log(JSON.stringify(tvSerie));
-
+// façon je pense un peu trop compliquée de le faire et pas adaptée a la 5-2.
 
 let serie;
 let production_year;
@@ -26,7 +31,7 @@ let castNumber;
 let castName = [];
 let cast = [];
 
-function askTvSerie(cast) {
+function askTvSerie() {
     this.serie = prompt('Nom de la série:');
     this.production_year = prompt('Année de production:');
     castNumber = prompt('Combien de membre du cast voulez vous entrez:')
@@ -39,11 +44,32 @@ function askTvSerie(cast) {
 }
 
 const tvSerie = new askTvSerie(serie, production_year, cast);
-// Traduis notre fonction en Objet avec l'opérateur new qui 
-const jsonSerie = (JSON.stringify(tvSerie));
+// Traduis notre fonction en Objet avec l'opérateur new qui.
+const jsonSerie = (tvSerie);
 // Crée une variable constante qui contient notre string JSON grace a la propriété JSON.stringify().
 
-console.log(JSON.parse(jsonSerie));
+console.log(jsonSerie);
 // je peu ajouter JSON.parse pour que ça s'affiche mieu au format json.
 
 // console.log(tvserie)
+
+
+
+
+// entrainement objet
+
+// let lobject = {
+//     array: "nul",
+//     array2: {
+//         array1: "eee",
+//         fdg: "hgjk",
+//         fdger: "hgjk",
+//         fdge: "hgjk",
+//     },
+// }
+
+// console.log(JSON.stringify(lobject))
+
+// const jsoned = JSON.stringify(lobject);
+
+// console.log(JSON.parse(jsoned))
--- 5-2.js
+++ 5-2.js
@@ -1,26 +1,27 @@
-let serie;
-let production_year;
-let castNumber;
-let castName = [];
-let cast = [];
+let serie, prod, castNumb, castName=[], cast=[]; 
 
-function askTvSerie(cast) {
-    this.serie = prompt('Nom de la série:');
-    this.production_year = prompt('Année de production:');
-    castNumber = prompt('Combien de membre du cast voulez vous entrez:')
+function askTvSerie(serie, prod, castNumb, cast) {
+    serie = prompt('Nom de votre série favorite?');
+    prod = prompt('Année de production de votre série favorite?');
+    castNumb = prompt('Combien de membre du cast voulez vous entrez?');
+
+    for (let i = 0; i < castNumb; i++) {
+        cast = prompt('Nom du cast?');
+        castName.push(cast)
+        cast = castName;
+    };
+
+    let tvSerie = {
+        serie: serie,
+        production_year: prod,
+        cast_member: cast,
+    };
+
+    return tvSerie;
+}
+
+console.log(askTvSerie());
+
+function randomizeCast(tvserie) {
     
-    for(let i = 0; i < castNumber; i++) {
-        cast = prompt('Membre du cast:');
-        castName.push(cast);
-        this.cast = castName;
-    }
 }
-
-const tvSerie = new askTvSerie(serie, production_year, cast);
-
-function randomizeCast(tvSerie) {
-    shuffle(cast)
-    console.log(JSON.stringify(tvSerie))
-}
-
-randomizeCast(tvSerie)
